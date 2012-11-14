e53ed6570cce1a342da4788de6b2ca1d3dcfb04c
Tony Baines <tony.baines@bt.com>
2012-11-14 15:24:37 +0000

    Acceptance test for story 1 (failing)

--- src/test/java/com/bt/tdd/shoppingcart/acceptance/AcceptanceTest.java
+++ src/test/java/com/bt/tdd/shoppingcart/acceptance/AcceptanceTest.java
@@ -3,19 +3,46 @@ package com.bt.tdd.shoppingcart.acceptance;
 import static org.junit.Assert.*;
 import static org.hamcrest.Matchers.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import com.bt.tdd.shoppingcart.UserInterface;
 
 public class AcceptanceTest {
 
-	@Test
-	public void shouldDisplayAGreetingMessage() {
-		UserInterface ui = new UserInterface();
-		assertThat(ui.getOutput(), allOf(
-				containsString("Welcome to The Shop In The Clouds"),
-				containsString("Your options are"),
-				containsString("Q: Quit")));
+	private UserInterface ui;
+
+	@Before
+	public void setUp() {
+		ui = new UserInterface();
 	}
 
+	@Test
+	public void shouldDisplayAGreetingMessage() {
+		assertThat(
+				ui.getOutput(),
+				allOf(containsString("Welcome to The Shop In The Clouds"),
+						containsString("Your options are"),
+						containsString("Q: Quit")));
+		quit();
+	}
+
+	@Test
+	public void allowsTheUserToSeeAListOfProducts() {
+		assertThat(ui.getOutput(),
+				containsString("L: List the Available Products"));
+		ui.sendInput("L");
+		assertThat(
+				ui.getOutput(),
+				allOf(containsString("[1] Fork Handles £11.99"),
+						containsString("[2] Candles £3.99"),
+						containsString("[3] Hoes £29.99")));
+
+		quit();
+	}
+
+	protected void quit() {
+		ui.sendInput("Q");
+		assertThat(ui.isActive(), is(false));
+	}
 }
